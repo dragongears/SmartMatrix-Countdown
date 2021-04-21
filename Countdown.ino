a4fad7c4390128621606b422e50d813fd6990872
Art Dahm <art@dahm.com>
2021-04-20 21:54:49 -0400

    Made changes for SmartMatrix4

--- Countdown.ino
+++ Countdown.ino
@@ -1,7 +1,7 @@
 /*
  * SmartMatrix Countdown
- * Version 0.1.2
- * Copyright (c) 2014 Art Dahm (art@dahm.com)
+ * Version 2.0.0
+ * Copyright (c) 2014-2021 Art Dahm (art@dahm.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -20,34 +20,75 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include <Arduino.h>
+#include <Wire.h>
 #include <Time.h>
-#include <SmartMatrix_32x32.h>
-#include "bitmap.c"
+#include <RTClib.h>
+#include <MatrixHardware_ESP32_V0.h>
+#include <SmartMatrix.h>
+#include "gencon.c"
 //#include "bitmap_sw.c"
 
+#define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
+const uint16_t kMatrixWidth = 32;       // Set to the width of your display, must be a multiple of 8
+const uint16_t kMatrixHeight = 32;      // Set to the height of your display
+const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
+const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
+const uint8_t kPanelType = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
+const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
+const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
+
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
+
+int brightness = 60;
+
+RTC_DS1307 rtc;
+
+
 // Change these variables for a new countdown
-tmElements_t eventDate = {0, 0, 0, 0, 3, 8, CalendarYrToTm(2021)};
+tmElements_t eventDate = {0, 0, 0, 0, 16, 9, CalendarYrToTm(2021)};
 char eventYear[] = "2021";
 
-rgb24 textColor = {0xfe, 0xd7, 0x1e};
+rgb24 textColor = {0xff, 0xe2, 0x3b};
 //rgb24 textColor = {0xf9, 0xff, 0xff};
 
 time_t eventTime = makeTime(eventDate);
 
-SmartMatrix matrix;
+// SmartMatrix matrix;
 
 void setup() {
-    setSyncProvider(getTeensy3Time);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Countdown");
 
-    matrix.begin();
-    matrix.setBrightness(128);
+    // setSyncProvider(getTeensy3Time);
+
+  Wire.begin(14, 13);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  matrix.addLayer(&backgroundLayer);
+  matrix.begin();
+  matrix.setBrightness(brightness*(255/100));
 
     int i;
     rgb24 *buffer;
 
     // Get the back buffer for the image
-    buffer = matrix.backBuffer();
+    buffer = backgroundLayer.backBuffer();
 
     // Copy the image into the buffer
     if ((bitmap_image.width <= matrix.getScreenWidth()) &&
@@ -59,7 +100,7 @@ void setup() {
         }
 
     // Show the image (swapBuffers is really a copy unless false is passed in)
-    matrix.swapBuffers(true);
+    backgroundLayer.swapBuffers(true);
 }
 
 void loop() {
@@ -68,10 +109,11 @@ void loop() {
     char days[] = "Days";
 
     // Get the number of days until the event
-    d = elapsedDays(eventTime) - elapsedDays(now());
+    DateTime now = rtc.now();
+    d = elapsedDays(eventTime) - elapsedDays(now.unixtime());
 
     // Clear the countdown area
-    matrix.fillRectangle(0, 21, 31, 31, {0x00, 0x00, 0x00});
+    backgroundLayer.fillRectangle(0, 21, 31, 31, {0x00, 0x00, 0x00});
 
     if (d > 0) {
         // Fill in the date string withe the number of days
@@ -84,42 +126,42 @@ void loop() {
 		        // Erase the 's' in 'Days' if only one day left
             days[3] = 0x00;
 
-            matrix.setFont(font8x13);
-            matrix.drawString(5, 20, textColor, &date[2]);
+            backgroundLayer.setFont(font8x13);
+            backgroundLayer.drawString(5, 20, textColor, &date[2]);
 
 	          // Draw the word 'Day'
-            matrix.setFont(font3x5);
-            matrix.drawString(15, 26, textColor, days);
+            backgroundLayer.setFont(font3x5);
+            backgroundLayer.drawString(15, 26, textColor, days);
         } else if (d <= 9) {
-            matrix.setFont(font8x13);
-            matrix.drawString(3, 20, textColor, &date[2]);
+            backgroundLayer.setFont(font8x13);
+            backgroundLayer.drawString(3, 20, textColor, &date[2]);
 
 		        // Draw the word 'Days'
-		        matrix.setFont(font3x5);
-		        matrix.drawString(13, 26, textColor, days);
+		        backgroundLayer.setFont(font3x5);
+		        backgroundLayer.drawString(13, 26, textColor, days);
         } else if (d <= 99) {
-            matrix.setFont(font6x10);
-            matrix.drawString(2, 22, textColor, &date[1]);
+            backgroundLayer.setFont(font6x10);
+            backgroundLayer.drawString(2, 22, textColor, &date[1]);
 
 		        // Draw the word 'Days'
-		        matrix.setFont(font3x5);
-		        matrix.drawString(15, 25, textColor, days);
+		        backgroundLayer.setFont(font3x5);
+		        backgroundLayer.drawString(15, 25, textColor, days);
         } else {
-            matrix.setFont(font5x7);
-            matrix.drawString(0, 24, textColor, date);
+            backgroundLayer.setFont(font5x7);
+            backgroundLayer.drawString(0, 24, textColor, date);
 
 		        // Draw the word 'Days'
-            matrix.setFont(font3x5);
-		        matrix.drawString(17, 25, textColor, days);
+            backgroundLayer.setFont(font3x5);
+		        backgroundLayer.drawString(17, 25, textColor, days);
         }
     } else {
         // Past the event date. Show the year in the countdown area
-        matrix.setFont(font8x13);
-        matrix.drawString(0, 20, textColor, eventYear);
+        backgroundLayer.setFont(font8x13);
+        backgroundLayer.drawString(0, 20, textColor, eventYear);
     }
 
     // Show the updated display
-    matrix.swapBuffers(false);
+    backgroundLayer.swapBuffers(false);
 
 
     // Wait before updating display
@@ -127,6 +169,6 @@ void loop() {
 
 }
 
-time_t getTeensy3Time() {
-    return Teensy3Clock.get();
-}
+// time_t getTeensy3Time() {
+//     return Teensy3Clock.get();
+// }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Countdown
 
-Count down the number of days to an event with the [Teensy 3.1](http://pjrc.com) and [SmartMatrix Shield](http://pixelmatix.com).
+Count down the number of days to an event.
 
 See the SmartMatrix BitmapTest example for how to created bitmaps for use with this program.
 
