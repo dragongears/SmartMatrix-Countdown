b23ecb24f6f3fa56f08cf0fc9508162a3b204a52
Art Dahm <art@dahm.com>
2023-05-06 16:32:33 -0400

    Draw days, weeks and days, weeks, or one week

--- Countdown.ino
+++ Countdown.ino
@@ -24,7 +24,6 @@
 #include "WiFi_Network.h"
 #include <WiFi.h>
 #include <TimeLib.h>
-#include <time.h>
 #include <MatrixHardware_ESP32_V0.h>
 #include <SmartMatrix.h>
 #include "gencon.c"
@@ -56,6 +55,8 @@ char eventYear[] = "2O23";
 rgb24 textColor = {0xff, 0x3b, 0xe2};
 
 time_t eventTime = mktime(&eventDate);
+
+uint loopCount = 0;
 
 void wifiConnect() {
   uint8_t count = 20;
@@ -127,6 +128,111 @@ void drawBitmap(int16_t x1, int16_t y1) {
   }
 }
 
+void drawDays(int16_t d) {
+  int16_t daysX = 0;
+  char date[] = "xxx";
+  char days[] = "DAYS";
+
+  // Fill in the date string withe the number of days
+  date[0] = '0' + d / 100;
+  date[1] = '0' + (d / 10) % 10;
+  date[2] = '0' + d % 10;
+
+  backgroundLayer.setFont(font8x13);
+
+  // Choose the font size based on the number of digits
+  if (d == 1) {
+    // Erase the 's' in 'Days' if only one day left
+    days[3] = 0x00;
+
+    backgroundLayer.drawString(15, 21, textColor, &date[2]);
+
+    // Draw the word 'Day'
+    daysX = 26;
+  } else if (d <= 9) {
+    backgroundLayer.drawString(12, 21, textColor, &date[2]);
+
+    // Draw the word 'Days'
+    daysX = 22;
+  } else if (d <= 99) {
+    backgroundLayer.drawString(6, 21, textColor, &date[1]);
+
+    // Draw the word 'Days'
+    daysX = 27;
+  } else {
+    backgroundLayer.drawString(3, 21, textColor, date);
+
+    // Draw the word 'Days'
+    daysX = 30;
+  }
+  // backgroundLayer.setFont(font3x5);
+  backgroundLayer.drawString(daysX, 21, textColor, days);
+}
+
+void drawWeeksDays(int16_t d) {
+  int16_t daysX = 0;
+  int16_t days = d % 7;
+  int16_t weeks = d / 7;
+  char day[] = "D";
+  char week[] = "W";
+  char date[] = "xx";
+
+  if (weeks < 10) {
+    daysX = -4;
+  }
+
+  date[0] = '0' + (weeks / 10) % 10;
+  date[1] = '0' + weeks % 10;
+  backgroundLayer.setFont(font8x13);
+  if (weeks < 10) {
+    backgroundLayer.drawString(18 + daysX, 21, textColor, &date[1]);
+  } else {
+    backgroundLayer.drawString(10 + daysX, 21, textColor, date);
+  }
+
+  backgroundLayer.setFont(font6x10);
+  backgroundLayer.drawString(28 + daysX, 24, textColor, week);
+
+  date[1] = '0' + days % 10;
+  backgroundLayer.setFont(font8x13);
+  backgroundLayer.drawString(39 + daysX, 21, textColor, &date[1]);
+  backgroundLayer.setFont(font6x10);
+  backgroundLayer.drawString(49 + daysX, 24, textColor, day);
+}
+
+void drawWeeks(int16_t d) {
+  int16_t daysX = 0;
+  int16_t weeksX = 0;
+  int16_t weeks = d / 7;
+  char week[] = "WEEKS";
+  char date[] = "xx";
+
+  if (weeks < 10) {
+    daysX = 3;
+    weeksX = -2;
+  }
+
+  date[0] = '0' + (weeks / 10) % 10;
+  date[1] = '0' + weeks % 10;
+  backgroundLayer.setFont(font8x13);
+
+  if (weeks < 10) {
+    backgroundLayer.drawString(2 + daysX, 21, textColor, &date[1]);
+  } else {
+    backgroundLayer.drawString(2 + daysX, 21, textColor, date);
+  }
+
+  // backgroundLayer.setFont(font6x10);
+  backgroundLayer.drawString(22 + weeksX, 21, textColor, week);
+}
+
+void drawOneWeek() {
+  backgroundLayer.setFont(font8x13);
+  backgroundLayer.drawString(15, 21, textColor, "1");
+  backgroundLayer.setFont(font6x10);
+  backgroundLayer.drawString(26, 24, textColor, "WEEK");
+}
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -152,9 +258,6 @@ void setup() {
 
 void loop() {
   int16_t d = 0;
-  int16_t daysX = 0;
-  char date[] = "xxx";
-  char days[] = "DAYS";
 
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
@@ -167,40 +270,15 @@ void loop() {
     backgroundLayer.fillRectangle(0, 21, 63, 31, {0x00, 0x00, 0x00});
 
     if (d > 0) {
-      // Fill in the date string withe the number of days
-      date[0] = '0' + d / 100;
-      date[1] = '0' + (d / 10) % 10;
-      date[2] = '0' + d % 10;
-
-      backgroundLayer.setFont(font8x13);
-
-      // Choose the font size based on the number of digits
-      if (d == 1) {
-        // Erase the 's' in 'Days' if only one day left
-        days[3] = 0x00;
-
-        backgroundLayer.drawString(15, 21, textColor, &date[2]);
-
-        // Draw the word 'Day'
-        daysX = 26;
-      } else if (d <= 9) {
-        backgroundLayer.drawString(12, 21, textColor, &date[2]);
-
-        // Draw the word 'Days'
-        daysX = 22;
-      } else if (d <= 99) {
-        backgroundLayer.drawString(6, 21, textColor, &date[1]);
-
-        // Draw the word 'Days'
-        daysX = 27;
+      if (d < 7 || loopCount / 4 % 2 == 0) {
+        drawDays(d);
+      } else if (d == 7) {
+        drawOneWeek();
+      } else if (d % 7 == 0) {
+        drawWeeks(d);
       } else {
-        backgroundLayer.drawString(3, 21, textColor, date);
-
-        // Draw the word 'Days'
-        daysX = 30;
+        drawWeeksDays(d);
       }
-      // backgroundLayer.setFont(font3x5);
-      backgroundLayer.drawString(daysX, 21, textColor, days);
     } else {
       // Past the event date. Show a message in the countdown area
       backgroundLayer.setFont(font5x7);
@@ -211,6 +289,8 @@ void loop() {
     backgroundLayer.swapBuffers(false);
 
     // Wait before updating display
-    delay(10000UL);
+    delay(1000UL);
   }
+
+  loopCount++;
 }
